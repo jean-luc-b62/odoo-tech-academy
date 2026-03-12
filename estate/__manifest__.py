a9669975a9568c4c3a9f1948920f6e894342d45d
pal <pal@odoo.com>
2026-03-12 10:41:36 +0100

    [IMP] estate: Add estate module skeleton

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "estate",
+    "version": "0.1",
+    "description": """
+        Module to deal with estate sales.
+    """,
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
+    "category": "Real Estate/Brokerage",
+    "data": [
+        # declare view, security and data files here
+    ],
+    "demo": [
+        # declare demo files here
+    ],
+    "application": True,
+}
