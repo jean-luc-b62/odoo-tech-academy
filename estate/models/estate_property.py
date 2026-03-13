469f8e997c7a2c7a53242cf266ece2a54fac5550
pal <pal@odoo.com>
2026-03-13 11:25:21 +0100

    [04][IMP] estate: Additional estate property attributes and fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,13 +8,21 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Datetime()
+    date_availability = fields.Datetime("Available From", copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(string="Status", default="new", copy=False, selection=[
+        ("new", "New"),
+        ("offer_received", "Offer Received"),
+        ("offer_accepted", "Offer Accepted"),
+        ("sold", "Sold"),
+        ("cancelled", "Cancelled"),
+    ])
