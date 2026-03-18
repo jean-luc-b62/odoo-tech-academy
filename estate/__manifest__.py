6b31c777ab7e0b19335656be374db294197bbfa5
yasffir <yasffir@icloud.com>
2026-03-18 11:53:41 +0100

    estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     "category": "Real Estate/Brokerage",
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "demo": [
         # declare demo files here
