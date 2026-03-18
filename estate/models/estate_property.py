1beb2d5006f00f2ef4915903216453dd3f29bdf0
yasffir <yasffir@icloud.com>
2026-03-18 14:43:35 +0100

    types and tags

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -45,3 +45,8 @@ class EstateProperty(models.Model):
         ],
         help="Orientation of the garden attached to this property.",
     )
+    property_type_id = fields.Many2one("estate.property.type")
+    buyer_id = fields.Many2one("res.partner")
+    seller_id = fields.Many2one("res.users", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
