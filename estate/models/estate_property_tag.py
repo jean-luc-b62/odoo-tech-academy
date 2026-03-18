bfb4fccf076fe8f0f68ba9927e5273f9bad3d544
yasffir <yasffir@icloud.com>
2026-03-18 16:28:14 +0100

    fix constrains

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -5,6 +5,8 @@ class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Tags of Properties"
 
-    _check_name_unique = models.Constraint("unique(name)", "The Tag's name must be unique.")
+    _sql_constraints = [
+        ("check_name_unique", "unique(name)", "The Tag's name must be unique."),
+    ]
 
     name = fields.Char(required=True)
