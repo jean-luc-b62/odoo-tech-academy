9f924414fac3937c250fccb7da0791c3034f5356
stenocyb <luxfuxnux@gmail.com>
2026-03-18 10:39:14 +0100

    Add fields to EstateProperty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,3 +5,16 @@ class EstateProperty(models.Model):
 	_description = "Estate Property Model"
 
 	name = fields.Char()
+	description = fields.Text()
+	postcode = fields.Char()
+	data_availability = fields.Datetime()
+	expected_price = fields.Float(required=True)
+	selling_price = fields.Float()
+	bedrooms = fields.Integer()
+	living_area = fields.Integer()
+	facades = fields.Integer()
+	garage = fields.Boolean()
+	garden = fields.Boolean()
+	garden_area = fields.Integer()
+	garden_orientation = fields.Selection([("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+	
