e0d68e852b1614fe55dc18f22a3486133800a10e
kjarret <jarrekenny@gmail.com>
2026-03-18 17:24:32 +0100

    Heritage

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,8 @@
 from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, fields, models
-
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError, UserError
+from odoo.tools.float_utils import float_compare, float_is_zero
 
 
 class EstateProperty(models.Model):
@@ -40,13 +40,21 @@ class EstateProperty(models.Model):
     total_area = fields.Float(compute="_compute_total_area")
     best_price = fields.Float(compute="_compute_best_price")
 
-    def action_sell_property(self):
-        if any(prop.state == "cancelled" for prop in self):
-            raise UserError(_("You cannot sell cancelled properties."))
-        self.state = "sold"
+    _check_expected_price_positive = models.Constraint(
+        "CHECK(expected_price > 0)",
+        "The Property's expected price must be positive."
+    )
+    _check_selling_price_positive = models.Constraint(
+        "CHECK(selling_price >= 0)",
+        "The Property's selling price must be positive."
+    )
 
-    def action_cancel_property(self):
-        self.state = "cancelled"
+    @api.constrains("expected_price", "selling_price")
+    def _check_prices(self):
+        for prop in self:
+            if not float_is_zero(prop.selling_price, precision_rounding=0.01) and \
+               float_compare(prop.selling_price, prop.expected_price * 0.9, precision_rounding=0.01) < 0:
+                raise ValidationError(_("The selling price cannot be lower than 90% of the expected price."))
 
     @api.depends("garden_area", "living_area")
     def _compute_total_area(self):
@@ -69,3 +77,16 @@ class EstateProperty(models.Model):
         else:
             self.garden_area = 0
             self.garden_orientation = False
+
+    @api.ondelete(at_uninstall=False)
+    def _unlink_if_new_cancelled(self):
+        if any(prop.state not in {"new", "cancelled"} for prop in self):
+            raise UserError(_("Only new or cancelled properties can be deleted."))
+
+    def action_sell_property(self):
+        if any(prop.state == "cancelled" for prop in self):
+            raise UserError(_("You cannot sell cancelled properties."))
+        self.state = "sold"
+
+    def action_cancel_property(self):
+        self.state = "cancelled"
